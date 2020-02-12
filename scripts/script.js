87ba051e37bad623b18f36c72bc4fe24316df8c6
Doudouthebadcat <dedieufabien@gmail.com>
2020-02-12 11:24:47 +0100

    script.js
    ajout d'un constructeur

--- scripts/script.js
+++ scripts/script.js
@@ -1,25 +1,24 @@
 class Voiture {
-  class Voiture {
-
-    vitesseMax = 0;
-    couleur = '';
-
-    accelerer() {
-      console.log('La voiture accélère !!');
-    }
-    freiner() {
-      console.log('La voiture freine...');
-    }
-
-  }
   vitesseMax = 0;
   couleur = '';
+  marque = '';
+
+  constructor(marque = '', vitesseMax = 0, couleur = ''){
+    this.vitesseMax = vitesseMax;
+    this.couleur = couleur;
+    this.marque = marque;
+    console.log(`Nouvelle voiture ${this.marque} de couleur ${this.couleur} créée. Elle va jusqu'à ${this.vitesseMax}km/h.`)
+  }
 
   accelerer() {
     console.log('La voiture accélère !!');
+    console.warn(`Attention : vitesse limitée à ${this.vitesseMax}km/h`);
   }
   freiner() {
     console.log('La voiture freine...');
   }
-
 }
+
+
+let voiture = new Voiture('bmw', 280, 'argent');
+voiture.accelerer();
