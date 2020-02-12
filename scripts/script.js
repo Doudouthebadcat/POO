f6a87b6e322d07abecb3b12f32577e65e7f9c747
Doudouthebadcat <dedieufabien@gmail.com>
2020-02-12 10:17:30 +0100

    index.html et script.js
    création index.html et script.js

---
+++ scripts/script.js
@@ -0,0 +1,25 @@
+class Voiture {
+  class Voiture {
+
+    vitesseMax = 0;
+    couleur = '';
+
+    accelerer() {
+      console.log('La voiture accélère !!');
+    }
+    freiner() {
+      console.log('La voiture freine...');
+    }
+
+  }
+  vitesseMax = 0;
+  couleur = '';
+
+  accelerer() {
+    console.log('La voiture accélère !!');
+  }
+  freiner() {
+    console.log('La voiture freine...');
+  }
+
+}
